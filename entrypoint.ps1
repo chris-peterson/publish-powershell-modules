279a7da39e262f7521b3ed0782066ef07af56fbd
Chris Peterson <chris.peterson@gettyimages.com>
2024-08-22 21:03:12 -0700

    Use newer, preferred publishing cmdlet
    https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.psresourceget/publish-psresource?view=powershellget-3.x#syntax

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,17 +1,50 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = 'Stop'
 
-$Modules = $null
-if ([string]::IsNullOrWhiteSpace($env:INPUT_MODULEPATH)) {
-    $Modules = Get-ChildItem -Recurse -Filter '*.psd1' |
-        Select-Object -Unique -ExpandProperty Directory
+# workaround for https://github.com/PowerShell/PSResourceGet/issues/1806
+Get-PSResourceRepository
+
+$ModuleManifests = if ([string]::IsNullOrWhiteSpace($env:INPUT_MODULEPATH)) {
+    Get-ChildItem -Recurse -Filter '*.psd1' | Select-Object -ExpandProperty FullName
 } else {
-    $Modules = @($env:INPUT_MODULEPATH)
+    @($env:INPUT_MODULEPATH)
 }
 
-$Modules | ForEach-Object {
-    Write-Host "Publishing '$_' to PowerShell Gallery"
+foreach ($ManifestPath in $ModuleManifests) {
+    $ModuleDir = Split-Path $ManifestPath -Parent
+    Write-Host "Processing module: $ManifestPath..."
 
-    Publish-Module -Path $_ -NuGetApiKey $env:INPUT_NUGETAPIKEY
-    Write-Host "'$_' published to PowerShell Gallery"
+    # by design, [Test-ModuleManifest](https://github.com/PowerShell/PowerShell/blob/master/src/System.Management.Automation/engine/Modules/TestModuleManifestCommand.cs#L196)
+    # will fail if a required module is not installed.  since we have no way of anticipating what modules an author might require
+    # we load and install everything first
+    $Manifest = Import-PowerShellDataFile -Path $ManifestPath
+    $RequiredModules = @()
+    if ($Manifest.RequiredModules) {
+        foreach ($Module in $Manifest.RequiredModules) {
+            if ($Module -is [string]) {
+                $RequiredModules += $Module
+            } elseif ($Module -is [hashtable]) {
+                $RequiredModules += $Module.ModuleName
+            }
+        }
+    }
+    foreach ($ModuleName in $RequiredModules) {
+        if (-not (Get-InstalledPSResource -Name $ModuleName -ErrorAction SilentlyContinue)) {
+            Write-Host "Installing required module: $ModuleName..."
+            Install-Module -Name $ModuleName -Scope CurrentUser -Force
+        }
+    }
+
+    Write-Host "Validating module manifest: $ManifestPath..."
+    Test-ModuleManifest -Path $ManifestPath
+
+    Write-Host "Publishing '$ModuleDir' to PowerShell Gallery..."
+    $Params = @{
+        ApiKey     = $env:INPUT_NUGETAPIKEY
+        Path       = $ModuleDir
+        Repository = 'PSGallery'
+        Verbose    = $true
+    }
+    Publish-PSResource @Params
+    Write-Host "...'$ModuleDir' published to PowerShell Gallery"
 }
